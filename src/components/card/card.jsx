5cf1b4558bea0b94f70f4e4e455522ca234ae8d1
Kitty Allen <kitty.allen@reedonline.co.uk>
2020-01-20 11:45:05 +0000

    updates the data to reflect airtable source

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -33,25 +33,86 @@ class Card extends React.Component {
   }
 
   render() {
-    const {
+    let {
       data: {
-        title,
-        description,
-        miles,
-        open,
-        hours,
-        voucherRequired,
-        town,
-        address,
-        phone,
-        email,
-        website
+        Name,
+        Description,
+        Monday_Open,
+        Monday_Close,
+        Tuesday_Open,
+        Tuesday_Close,
+        Wednesday_Open,
+        Wednesday_Close,
+        Thursday_Open,
+        Thursday_Close,
+        Friday_Open,
+        Friday_Close,
+        Saturday_Open,
+        Saturday_Close,
+        Sunday_Open,
+        Sunday_Close,
+        Requires_Voucher,
+        Address_Line_1,
+        Address_Line_2,
+        Address_Line_3,
+        Postcode,
+        Phone,
+        Email,
+        Website,
+        FoodCentre
       }
     } = this.props;
     const { isCardExtended, voucherModalOpen } = this.state;
+    const hours = {
+      mon: `${Monday_Open} - ${Monday_Close}`,
+      tue: `${Tuesday_Open} - ${Tuesday_Close}`,
+      wed: `${Wednesday_Open} - ${Wednesday_Close}`,
+      thurs: `${Thursday_Open} - ${Thursday_Close}`,
+      fri: `${Friday_Open} - ${Friday_Close}`,
+      sat: `${Saturday_Open} - ${Saturday_Close}`,
+      sun: `${Sunday_Open} - ${Sunday_Close}`
+    };
 
+    if(Monday_Open === 'Closed') {
+      hours.mon = 'Closed'
+    }
+    if(Tuesday_Open === 'Closed') {
+      hours.tue = 'Closed'
+    }
+    if(Wednesday_Open === 'Closed') {
+      hours.wed = 'Closed'
+    }
+    if(Thursday_Open === 'Closed') {
+      hours.thurs = 'Closed'
+    }
+    if(Friday_Open === 'Closed') {
+      hours.fri = 'Closed'
+    }
+    if(Saturday_Open === 'Closed') {
+      hours.sat = 'Closed';
+    }
+    if(Sunday_Open === 'Closed') {
+      hours.sun = 'Closed';
+    }
+
+    if (Requires_Voucher === 'Yes') {
+      Requires_Voucher = true
+    } else {
+      Requires_Voucher = false
+    }
+
+    if(FoodCentre === 'true') {
+      FoodCentre = true
+    } else {
+      FoodCentre = false
+    }
+
+    let adviceCentreStyle = '';
+    if (!FoodCentre) {
+      adviceCentreStyle = 'advice-centre'
+    }
     return (
-      <div className="card-container">
+      <div className={`card-container ${adviceCentreStyle}`}>
         {voucherModalOpen && (
           <React.Fragment>
             <div className="overlay" onClick={this.handleOverlayClick} />
@@ -80,19 +141,19 @@ class Card extends React.Component {
             </div>
           </React.Fragment>
         )}
-        <div className="tag-miles">{miles} miles</div>
+        {/* <div className="tag-miles">{miles} miles</div> */}
         <div className="card-heading-container">
-          <h3 className="heading">{title}</h3>
-          {open && <span className="tag-open">Open</span>}
+          <h3 className="heading">{Name}</h3>
+          {/* {open && <span className="tag-open">Open</span>} */}
         </div>
 
-        <p>{description}</p>
-        {voucherRequired && (
+        <p>{Description}</p>
+        {Requires_Voucher && (
           <button className="card-voucher" onClick={this.handleVoucherClick}>
             Voucher required
           </button>
         )}
-        {!isCardExtended && <p>{town}</p>}
+        {!isCardExtended && <p>{Address_Line_3}</p>}
         <button onClick={this.handleClick} className="card-extend-toggle">
           {isCardExtended ? <ChevronUp /> : <ChevronDown />}
           See more
@@ -101,11 +162,14 @@ class Card extends React.Component {
         {isCardExtended && (
           <div className="card-extended-container">
             <div>
-              <p>{address}</p>
+              <p>{Address_Line_1}</p>
+              <p>{Address_Line_2}</p>
+              <p>{Address_Line_3}</p>
+              <p>{Postcode}</p>
               <ul>
-                <li>{phone}</li>
-                <li>{email}</li>
-                <li>{website}</li>
+                <li>{Phone}</li>
+                <li>{Email}</li>
+                <li>{Website}</li>
               </ul>
             </div>
             <div>
@@ -114,7 +178,7 @@ class Card extends React.Component {
                 <li>Mon: {hours.mon}</li>
                 <li>Tue: {hours.tue}</li>
                 <li>Wed: {hours.wed}</li>
-                <li>Thurs: {hours.thur}</li>
+                <li>Thurs: {hours.thurs}</li>
                 <li>Fri: {hours.fri}</li>
                 <li>Sat: {hours.sat}</li>
                 <li>Sun: {hours.sun}</li>
