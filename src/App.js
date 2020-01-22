01b16024a11e57e65e01182301a83d0dc3a6beac
Kitty Allen <kitty.allen@reedonline.co.uk>
2020-01-22 17:57:38 +0000

    updates the nav links

--- src/App.js
+++ src/App.js
@@ -24,35 +24,62 @@ class App extends React.Component {
       });
   }
   render() {
-    const {records} = this.state;
+    const { records } = this.state;
     return (
       <div className="App">
         <header className="navbar">
           <ul>
-            <img src={LLLogo} alt="lambeth larder icon" className='ll_logo' />
+            <a href="http://www.lambethlarder.org/">
+              <img src={LLLogo} alt="lambeth larder icon" className="ll_logo" />
+            </a>
           </ul>
-          <ul>
-            <li>Home</li>
-            <li>Emergency Food</li>
-            <li>Advice & support</li>
-            <li>Money saving ideas</li>
-            <li>Mental health</li>
-            <li>Blog</li>
-            <li>About us</li>
+          <ul className='navLinksContainer'>
+            <li>
+              <a  className='linkItem' href="http://www.lambethlarder.org/">Home</a>
+            </li>
+            <li>
+              <a href="http://www.lambethlarder.org/emergency-food.html">
+                Emergency Food
+              </a>
+            </li>
+            <li>
+              <a href="http://www.lambethlarder.org/advice--support.html">
+                Advice & support
+              </a>
+            </li>
+            <li>
+              <a href="http://www.lambethlarder.org/money-saving-ideas.html">
+                Money saving ideas
+              </a>
+            </li>
+            <li>
+              <a href="http://www.lambethlarder.org/mental-health.html">
+                Mental health
+              </a>
+            </li>
+            <li>
+              <a href="http://www.lambethlarder.org/food-growing.html">
+                Food Growing
+              </a>
+            </li>
+            <li>
+              <a href="http://www.lambethlarder.org/blog">Blog</a>
+            </li>
+            <li>
+              <a href="http://www.lambethlarder.org/about-us.html">About us</a>
+            </li>
           </ul>
         </header>
-        <div className='hero' />
+        <div className="hero" />
 
         <main>
           {this.state.records.length > 0 ? (
             <>
               <Map />
-              <List results={records}/>
+              <List results={records} />
             </>
           ) : (
-            <div>
-              Loading...
-            </div>
+            <div>Loading...</div>
           )}
         </main>
       </div>
